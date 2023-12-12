db7e593b5a99173bf0124b7f75b0da3c3a9ac00e
Prempavan Gunda <prempavan_gunda@thbs.india.com>
2023-12-12 10:41:01 +0530

    file1.java is added

---
+++ file1.java
@@ -0,0 +1,32 @@
+/*package whatever //do not write package name here */
+
+import java.io.*;
+
+import java.util.Scanner;
+import java.util.*;
+
+public class file1 {
+
+public static void main(String[] args) {
+
+	int number =45;
+
+	
+
+	if (number > 0) {
+
+	System.out.println(number + " is positive.");
+
+	} else if (number < 0) {
+
+	System.out.println(number + " is negative.");
+
+	} else {
+
+	System.out.println(number + " is zero.");
+
+	}
+
+}
+
+}
